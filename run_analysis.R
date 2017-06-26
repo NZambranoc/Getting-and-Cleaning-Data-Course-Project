25cdf2745be5bb066f379398d5c47b6442621f98
NZambranoc <n.zambranocast@gmail.com>
2017-06-26 03:49:31 -0400

    Added step 3
    Moved desriptive label substitution for "activity" variable to step (3).

--- run_analysis.R
+++ run_analysis.R
@@ -9,10 +9,6 @@ library(dplyr)
 #Load feature variable names tables
 varnames <- read_table2("UCI HAR Dataset/features.txt",col_names = F, col_types = list("c","c"))
 varnames <- unlist(varnames[,2],use.names = F)
-#Load activity lables factor
-activitylabels <- read_table2("UCI HAR Dataset/activity_labels.txt",col_names = F,col_types = "ic")
-activitylabels <- unlist(activitylabels[,2],use.names = F)
-activitylabels <- factor(activitylabels, levels = activitylabels)
 
 ##### Load Test dataset===================
 #Load test dataset subject identifier
@@ -24,7 +20,7 @@ test_activitylabel <- unlist(test_activitylabel, use.names = F)
 #Load test dataset feature  values
 test <- suppressMessages(suppressWarnings(read_table2("UCI HAR Dataset/test/X_test.txt", col_names = varnames)))
 #Bind test records to test_subjectid and test_activitylabel
-test <- cbind(subjectid=test_subjectid, test, activity=activitylabels[test_activitylabel])
+test <- cbind(subjectid=test_subjectid, test, activity=test_activitylabel)
 
 ##### Load train dataset====================
 #Load train dataset subject identifier
@@ -39,14 +35,20 @@ train <- suppressMessages(suppressWarnings(
     read_table2("UCI HAR Dataset/train/X_train.txt", col_names = varnames)
 ))
 #bind values to subject identifier
-train <- cbind(subjectid=train_subjectid, train,activity=activitylabels[train_activitylabel])
+train <- cbind(subjectid=train_subjectid, train,activity=train_activitylabel)
 
 #####Bind test and train datasets -----
 HAR <- rbind(test,train)
-
-
 
 ##2) Extracts only the measurements on the mean and standard deviation for each measurement.####
 meancols <- varnames[grep(pattern = "mean()",varnames)]
 stdcols <- varnames[grep(pattern = "std()",varnames)]
 HAR_mean_std <- HAR[c("subjectid",meancols,stdcols,"activity")]
+
+##3) Uses descriptive activity names to name the activities in the data set ####
+#####Load activity lables factor
+activitylabels <- read_table2("UCI HAR Dataset/activity_labels.txt",col_names = F,col_types = "ic")
+activitylabels <- unlist(activitylabels[,2],use.names = F)
+activitylabels <- factor(activitylabels, levels = activitylabels)
+#####Substitute activity variable integers for corresponding descriptive activity label
+HAR <- activitylabels[HAR$activity]
